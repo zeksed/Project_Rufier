1d92f720a08464561ba948f69686d78feae30400
Nikita <106909320+zeksed@users.noreply.github.com>
2022-06-05 17:53:14 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,1 +1,6 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGroupBox, QRadioButton, QListWidget, QLineEdit, QGridLayout, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFontclass Second_Window(QWidget):
+class Second_Window(QWidget):    
+    def __init__(self):
+        super().__init__()
