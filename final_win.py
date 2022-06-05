3d1fee7b4f753733c1a1feac539a616de5308d05
Nikita <106909320+zeksed@users.noreply.github.com>
2022-06-05 17:52:33 +0700

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,1 +1,6 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGroupBox, QRadioButton, QListWidget, QLineEdit, QGridLayout, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
+class Final_Window(QWidget):
+    def __init__():
+        super().__init__()
