cd70e214044d90a01521e4dc8183c26e32ec15c7
Nikita <106909320+zeksed@users.noreply.github.com>
2022-06-12 17:23:03 +0700

    0.5v

--- final_win.py
+++ final_win.py
@@ -1,6 +1,23 @@
-from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGroupBox, QRadioButton, QListWidget, QLineEdit, QGridLayout, QPushButton, QVBoxLayout, QHBoxLayout
-from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
-class Final_Window(QWidget):
-    def __init__():
+from PyQT5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
+from instr import *
+
+class FinalWin(QWidget):
+    def __init__(self):
         super().__init__()
+        self.initUI()
+        self.set_appear()
+        self.show()
+    def initUI(self):
+        self.workh_text = QLabel(txt_workheart)
+        self.index_text = QLabel(txt_index)
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(index_text, alignment=Qt.AlingCenter)
+        self.layout_line.addWidget(workh_text, alignment=Qt.AlingCenter)
+
+        self.setlayout(self.layout_line)
+    def set_appear(self):
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width,win_height)
+        self.move(win.x, win.y)
