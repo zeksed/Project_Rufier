e06dfccd384b9740739dc1feac37b9fb75de9e7e
Nikita <106909320+zeksed@users.noreply.github.com>
2022-06-19 17:03:41 +0700

    0.6v

--- second_win.py
+++ second_win.py
@@ -1,8 +1,15 @@
-from PyQT5.QtCore import Qt
+from PyQT5.QtCore import Qt, QTime, QTimer, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
 from instr import *
 from final_win import *
 
+class Experiment():
+    def __init__(self, age, test1, test2, test3):
+        self.age = age
+        self.t1 = test1
+        self.t2 = test2
+        self.t3 = test3
 class Second_Win(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,11 +17,11 @@ class Second_Win(QWidget):
         self.connects()
         self.set_appear()
         self.show()
-    def next_click(self):
-        self.tw = Second_Win()
-        self.hide()
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
+        self.btn_test1.clicked.connect(self.timer_test)
+        self.btn_test2.clicked.connect(self.timer_sits)
+        self.btn_test3.clicked.connect(self.timer_final)
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
@@ -63,10 +70,49 @@ class Second_Win(QWidget):
         self.h_line.addLayout(self.r_line)
     def next_click(self):
         self.hide()
+        self.exp = Experiment(int(self.line_age.text()), self.line_test1.text(), self.line_test2.text(), self.line_test3.text())
         self.fw = FinalWin()
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
+    def timer_test(self):
+        global time
+        time = QTime(0,0,15)
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1500)
+    def timer_sits(self):
+        global time
+        time = QTime(0,0,30)
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+    def timer_final(self):
+        global time
+        time = QTime(0,1,0)
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"[6:8]))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) >= 15:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        else:
+            self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
